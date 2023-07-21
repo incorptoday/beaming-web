4d52280d98e5f09106cfb84a6cc69dbfa620d695
DuongKien <ddk0705@gmail.com>
2023-07-21 17:05:30 +0700

    remove links

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,14 +6,14 @@ const Home = () => {
       <div className="home">
         <h1>Beem</h1>
 
-        <h3>Coming Soon</h3>
+        <h3>Beaming App</h3>
 
         <div className="home__btns">
-          <a href="#">
+          <a>
             <img alt="image" src={require('../../assets/images/googleplay.png')} />
           </a>
 
-          <a href="#">
+          <a>
             <img alt="image" src={require('../../assets/images/appstore.png')} />
           </a>
         </div>
