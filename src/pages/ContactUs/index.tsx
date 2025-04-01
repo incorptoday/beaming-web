015bedac30f1c6582ecee805a93846aa71b25e14
DuongKien <ddk0705@gmail.com>
2025-04-01 16:41:24 +0700

    update styles

--- src/pages/ContactUs/index.tsx
+++ src/pages/ContactUs/index.tsx
@@ -1,5 +1,5 @@
-import { Fragment } from 'react';
 import { Button, Form, Input, message } from 'antd';
+import { Fragment, useState } from 'react';
 import HTTP from '../../core/http';
 
 const { TextArea } = Input;
@@ -7,22 +7,30 @@ const { TextArea } = Input;
 const ContactUs = () => {
   const [form] = Form.useForm();
 
+  const [loading, setLoading] = useState<boolean>(false);
+
   const resetForm = () => {
     form.resetFields();
-  }
+  };
 
   const onSubmit = async (values: any) => {
     const valid = await form.validateFields();
     if (!valid) return;
 
     try {
+      setLoading(true);
       await HTTP.post('/contact', values);
-      message.success('Thank you for reaching out! Someone will be in touch with you shortly.');
+      message.success({
+        content:
+          'Thank you for reaching out! Someone will be in touch with you shortly.',
+      });
       resetForm();
     } catch (e) {
       console.log(e);
+    } finally {
+      setLoading(false);
     }
-  }
+  };
 
   return (
     <Fragment>
@@ -46,7 +54,10 @@ const ContactUs = () => {
               rules={[
                 { required: true, message: 'First name is required' },
                 { min: 2, message: 'First name must be at least 2 characters' },
-                { max: 100, message: 'First name must be no more than 100 characters' }
+                {
+                  max: 100,
+                  message: 'First name must be no more than 100 characters',
+                },
               ]}
             >
               <Input />
@@ -58,7 +69,10 @@ const ContactUs = () => {
               rules={[
                 { required: true, message: 'Last name is required' },
                 { min: 2, message: 'Last name must be at least 2 characters' },
-                { max: 100, message: 'Last name must be no more than 100 characters' }
+                {
+                  max: 100,
+                  message: 'Last name must be no more than 100 characters',
+                },
               ]}
             >
               <Input />
@@ -69,7 +83,12 @@ const ContactUs = () => {
               name="email"
               rules={[
                 { required: true, message: 'Email is required' },
-                { pattern: new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'), message: 'Please enter valid email' },
+                {
+                  pattern: new RegExp(
+                    '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$',
+                  ),
+                  message: 'Please enter valid email',
+                },
               ]}
             >
               <Input />
@@ -78,21 +97,21 @@ const ContactUs = () => {
             <Form.Item
               label="Message"
               name="message"
-              rules={[
-                { required: true, message: 'Message is required' },
-              ]}
+              rules={[{ required: true, message: 'Message is required' }]}
             >
               <TextArea />
             </Form.Item>
 
             <Form.Item>
-              <Button htmlType="submit">Submit</Button>
+              <Button type="primary" htmlType="submit" loading={loading}>
+                Submit
+              </Button>
             </Form.Item>
           </Form>
         </div>
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
 export default ContactUs;
